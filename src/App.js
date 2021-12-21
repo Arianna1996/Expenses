fe6bb7a85e5488186a5e9ba753be6f8fcb6e92a0
Arianna Pavan <ariannapavan@MBP-di-Arianna.fritz.box>
2021-12-21 13:00:30 +0100

    Filtrato spese

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 305.89,
-      date: new Date(2021, 11, 10),
-    },
-    {
-      id: "e2",
-      title: "Crocchette",
-      amount: 10.5,
-      date: new Date(2020, 10, 10),
-    },
-    {
-      id: "e3",
-      title: "Bread",
-      amount: 5.6,
-      date: new Date(2020, 9, 30),
-    },
-    {
-      id: "e4",
-      title: "Water",
-      amount: 2.5,
-      date: new Date(2021, 5, 5),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 305.89,
+    date: new Date(2021, 11, 10),
+  },
+  {
+    id: "e2",
+    title: "Crocchette",
+    amount: 10.5,
+    date: new Date(2020, 10, 10),
+  },
+  {
+    id: "e3",
+    title: "Bread",
+    amount: 5.6,
+    date: new Date(2020, 9, 30),
+  },
+  {
+    id: "e4",
+    title: "Water",
+    amount: 2.5,
+    date: new Date(2021, 5, 5),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log("Siamo in App.js");
-    console.log(expense);
+    //console.log("Siamo in App.js");
+    //console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
@@ -38,6 +44,6 @@ function App() {
       <Expenses expenses={expenses} />;
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,31 +9,21 @@ const Expenses = (props) => {
   const nuovaData = (data) => {
     setData(data);
   };
+  const datoFiltrato = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === data
+  );
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter onCambiaData={nuovaData} selected={data} />
-        <p>{data}</p>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {datoFiltrato.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
